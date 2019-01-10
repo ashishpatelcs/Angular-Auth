8c21c4aacb14822e2d10bdd5cc82c53dbd8a8059
Ashish Patel <ashishpatel@skaas.guru>
2019-01-10 17:00:50 +0530

    added json web token for authentication

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user')
 
@@ -24,7 +25,10 @@ router.post('/register', (req, res) => {
         (err, user) => {
             if (err) console.log('error occurred');
             else {
-                res.status(200).send(user);
+                let payload = { subject: user._id }
+                let token = jwt.sign(payload, 'secretkey')
+                console.log(token);
+                res.status(200).send({ token });
             }
         }
     );
@@ -44,7 +48,9 @@ router.post('/login', (req, res) => {
                 if (user.password != userData.password) {
                     res.status(401).send('Invalid password');
                 } else {
-                    res.status(200).send(user);
+                    let payload = { subject: user._id };
+                    let token = jwt.sign(payload, 'secretkey');
+                    res.status(200).send({ token });
                 }
             }
         }
